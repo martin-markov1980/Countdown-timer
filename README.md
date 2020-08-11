54fe175680998df6917e101d7fcc489a5a83a5f4
Martin Markov <martin.markov@weareaccess.co.uk>
2020-08-11 16:45:38 +0100

    Initialized countdown timer.

---
+++ README.md
@@ -0,0 +1,6 @@
+# Countdown timer
+This is responsive countdown timer builded for a client.<br>
+Used technologies:
+  - HTML5 
+  - CSS3 including Flex Box 
+  - Javascript
---
+++ js/script.js
@@ -0,0 +1,34 @@
+// Set the date we're counting down to
+const countDownDate = new Date("December 11, 2020 15:29:59").getTime();
+
+// Update the count down every 1 second
+let x = setInterval(function() {
+
+  // Get today's date and time
+  let now = new Date().getTime();
+    
+  // Find the distance between now and the count down date
+  let distance = countDownDate - now;
+    
+  // Time calculations for days, hours, minutes and seconds
+  let days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  let hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+    
+  // Output the result in an elements with id="days" id="hours" id="minutes" id="seconds"
+	document.getElementById("days").innerHTML = days;
+	document.getElementById("hours").innerHTML = hours;
+	document.getElementById("minutes").innerHTML = minutes;
+	document.getElementById("seconds").innerHTML = seconds;
+    
+  // If the count down is over, write some text and set all the digits to 0
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("registration").innerHTML = "Registration Expired: ";
+		document.getElementById("days").innerHTML = 0;
+		document.getElementById("hours").innerHTML = 0;
+		document.getElementById("minutes").innerHTML = 0;
+		document.getElementById("seconds").innerHTML = 0;
+  }
+}, 1000);
