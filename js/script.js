17d31f402612dea61971a6ee931947059316fee7
Martin Markov <martin.markov@weareaccess.co.uk>
2020-08-12 09:31:32 +0100

    Changed variables to use let instead of var.

--- js/script.js
+++ js/script.js
@@ -14,7 +14,7 @@ let x = setInterval(function() {
   let days = Math.floor(distance / (1000 * 60 * 60 * 24));
   let hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
   let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+  let seconds = Math.floor((distance % (1000 * 60)) / 1000);
     
   // Output the result in an elements with id="days" id="hours" id="minutes" id="seconds"
 	document.getElementById("days").innerHTML = days;
